bb8525d0cfe0d2098fc08ae50d7c567a552337ad
root <root@ip-172-31-13-9.eu-west-1.compute.internal>
2023-10-21 16:13:01 +0300

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { MenuService } from '../shared/menu/menu.service';
 import { MenuID } from '../shared/menu/menu-id.model';
 import { LocaleService } from '../core/locale/locale.service';
-
+import { HostListener } from '@angular/core';
 /**
  * Represents the header with the logo and simple navigation
  */
@@ -13,6 +13,9 @@ import { LocaleService } from '../core/locale/locale.service';
   templateUrl: 'header.component.html',
 })
 export class HeaderComponent {
+
+  handleScroll = new BehaviorSubject<boolean>(false);
+  topPosToStartShowing = 50;
   /**
    * Whether user is authenticated.
    * @type {Observable<string>}
@@ -29,5 +32,20 @@ export class HeaderComponent {
 
   public toggleNavbar(): void {
     this.menuService.toggleMenu(this.menuID);
+  }
+
+  @HostListener('window:scroll')
+  checkScroll(){
+    const scrollPosition =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+
+    if (scrollPosition > this.topPosToStartShowing) {
+      this.handleScroll.next(true);    }
+    else {
+      this.handleScroll.next(false)
+    }
   }
 }
