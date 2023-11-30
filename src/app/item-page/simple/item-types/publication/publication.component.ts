d5a1eb9e861225a03da8318b8557f5444bcb026a
root <root@ip-172-31-13-9.eu-west-1.compute.internal>
2023-11-30 18:07:01 +0300

    first commit

--- src/app/item-page/simple/item-types/publication/publication.component.ts
+++ src/app/item-page/simple/item-types/publication/publication.component.ts
@@ -2,9 +2,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ViewMode } from '../../../../core/shared/view-mode.model';
 import { listableObjectComponent } from '../../../../shared/object-collection/shared/listable-object/listable-object.decorator';
 import { ItemComponent } from '../shared/item.component';
-import { isAuthenticated } from 'src/app/core/auth/selectors';
-import { select } from '@ngrx/store';
-import { Observable } from 'rxjs';
 
 /**
  * Component that represents a publication Item page
@@ -18,9 +15,6 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PublicationComponent extends ItemComponent {
-  public isAuthenticated$: Observable<boolean>;
-ngOnInit(): void {
-  this.isAuthenticated$ = this.store.pipe(select(isAuthenticated));
-}
+
 
 }
